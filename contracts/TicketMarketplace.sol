fec63f1f326ac0d6fb013e282f001fad1600e4b7
alanyeung95 <45751387+alanyeung95@users.noreply.github.com>
2024-02-03 18:55:09 -0500

    wip: Finished several functions

--- contracts/TicketMarketplace.sol
+++ contracts/TicketMarketplace.sol
@@ -15,24 +15,63 @@ contract TicketMarketplace is ITicketMarketplace {
     address public nftContract;
     uint128 public currentEventId = 0;
 
+    struct Event {
+        uint128 nextTicketToSell;
+        uint128 maxTickets;
+        uint256 pricePerTicket;
+        uint256 pricePerTicketERC20;
+    }
+
+    mapping(uint128 => Event) public events;
+    
     constructor(address _erc20TokenAddress) {
         ERC20Address = _erc20TokenAddress;  // set ERC20 token address
-        ticketNFT = new TicketNFT(""); // to-ask: empty string?
+        ticketNFT = new TicketNFT(""); 
         nftContract = address(ticketNFT);
         owner = msg.sender;
     }
 
     function createEvent(uint128 maxTickets, uint256 pricePerTicket, uint256 pricePerTicketERC20) external override {
-   }
+        require(msg.sender == owner, "Unauthorized access");
+
+        Event memory newEvent = Event({
+            nextTicketToSell: 0,
+            maxTickets: maxTickets,
+            pricePerTicket: pricePerTicket,
+            pricePerTicketERC20: pricePerTicketERC20
+        });
+
+        events[currentEventId] = newEvent;
+
+        emit EventCreated(currentEventId, maxTickets, pricePerTicket, pricePerTicketERC20);
+
+        currentEventId++;
+    }
 
     function setMaxTicketsForEvent(uint128 eventId, uint128 newMaxTickets) external override {
+        require(msg.sender == owner, "Unauthorized access");
+        require(newMaxTickets >= events[eventId].maxTickets, "The new number of max tickets is too small!");
+
+        events[eventId].maxTickets = newMaxTickets;
+
+        emit MaxTicketsUpdate(eventId, newMaxTickets);
     }
 
     function setPriceForTicketETH(uint128 eventId, uint256 price) external override {
+        require(msg.sender == owner, "Unauthorized access");
+
+        events[eventId].pricePerTicket = price;
+
+       emit PriceUpdate(eventId, price, "ETH");
     }
 
     function setPriceForTicketERC20(uint128 eventId, uint256 price) external override {
-     }
+        require(msg.sender == owner, "Unauthorized access");
+
+        events[eventId].pricePerTicketERC20 = price;
+
+        emit PriceUpdate(eventId, price, "ERC20");
+    }
 
     function buyTickets(uint128 eventId, uint128 ticketCount) payable external override {
     }
@@ -41,8 +80,13 @@ contract TicketMarketplace is ITicketMarketplace {
     }
 
     function setERC20Address(address newERC20Address) external override {
+        require(msg.sender == owner, "Unauthorized access");
+
+        ERC20Address = newERC20Address;
+
+        emit ERC20AddressUpdate(newERC20Address);
     }
 
     function _processTicketPurchase(uint128 eventId, uint128 ticketCount) private {
-   }
+    }
 }
